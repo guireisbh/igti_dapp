81064a5c437cc2fd286fcfa204e21d65ea2e8975
Guilherme Reis <7688223+guireisbh@users.noreply.github.com>
2020-04-15 20:36:54 -0300

    Create atividade_previa.sol

---
+++ atividade_previa.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.9;
+
+contract IGTI {
+    string public objetivo;
+    
+    constructor (string memory _objetivo) public {
+        objetivo = _objetivo;
+    }
+
+    function soma(int256 valor1, int256 valor2) public pure returns(int256){
+        return valor1 + valor2;
+    }
+    
+}
